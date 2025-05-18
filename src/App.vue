c2528a66e8c66bcdd14ac1b7b9fab4e7381d9857
= <=>
2025-05-18 20:17:54 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Banner />
+</template>
+
+<script lang="ts">
+import Banner from "./assets/components/Banner.vue";
+export default {
+  components: { Banner },
+};
+</script>
+
+<style></style>
---
+++ src/assets/components/Banner.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="header">
+    <div>
+      <div class="apresentacao">
+        <img src="../image/logo.svg" alt="Logo" class="logo" />
+      </div>
+      <div>
+        <p class="cabecalho-lg frase-cabecalho">
+          <span class="texto-verde">Um banquete de ideias para</span>
+          despertar o chef que há em você!
+        </p>
+        <p class="subtitulo-lg">
+          Explore novas receitas todos os dias com os ingredientes que estão ao
+          seu alcance!
+        </p>
+      </div>
+    </div>
+    <div>
+      <img class="foto-banner" src="../image/foto-banner.png" alt="Banner" />
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+  name: "BannerComponent",
+};
+</script>
+
+<style scoped>
+.header {
+  display: grid;
+  margin: 0 auto;
+  max-width: 1200px;
+  grid-template-columns: 580px 1fr;
+  gap: 10rem;
+  margin-top: 4rem;
+}
+.apresentacao {
+  display: flex;
+  flex-direction: row;
+}
+</style>

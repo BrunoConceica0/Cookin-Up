d659203f835b87418f4d9ff979092df0e58c4c77
Bruno <bd687353@gmail.com>
2025-07-18 11:20:31 -0300

    [Demo Task] Refactor the biggest file in the codebase

--- src/assets/components/ShowRecipes.vue
+++ src/assets/components/ShowRecipes.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    <h2>Show Recipes</h2>
-  </div>
+  <section>
+    <span class="subtitulo-lg sua-lista-texto">Receitas</span>
+    <ul v-if="ingredientes.length > 0" class="ingredientes-sua-lista">
+      <li v-for="(ingrediente, index) in ingredientes" :key="index">
+     
+      </li>
+    </ul>
+    <p v-else class="paragrafo lista-vazia">
+      <img src="../image/icones/lista-vazia.svg" alt="Ícone de pesquisa" />
+      Sua lista está vazia, selecione ingredientes para iniciar.
+    </p>
+  </section>
 </template>
 
 <script lang="ts">
-export default {};
+import { getCategories } from "@/http/index";
+import type ICategories from "@/interfaces/ICategories";
+
+export default {
+  data() {
+    return {
+      ingredientes: [] as ICategories[],
+    };
+  },
+  async created() {
+    this.ingredientes = await getCategories();
+  },
+};
 </script>
 
 <style></style>
